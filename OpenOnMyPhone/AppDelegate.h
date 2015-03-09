8b5e15f0df7f4bec7f4c3239849758d58ba56dac
Illvili <rainyloved@gmail.com>
2015-03-09 11:45:49 +0800

    Initial Commit

---
+++ OpenOnMyPhone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenOnMyPhone
+//
+//  Created by Illvili on 15/3/9.
+//  Copyright (c) 2015年 Illvili.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenOnMyPhone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenOnMyPhone
+//
+//  Created by Illvili on 15/3/9.
+//  Copyright (c) 2015年 Illvili.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
